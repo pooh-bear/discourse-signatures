63056915c1cd83ac0e9577303d9b8a4cf044b9c1
Gabe Palomares <dev@gabe.pw>
2019-04-14 23:38:42 +1000

    (feat): show sig unless user opts to disable it

--- plugin.rb
+++ plugin.rb
@@ -6,17 +6,17 @@
 
 enabled_site_setting :signatures_enabled
 
-DiscoursePluginRegistry.serialized_current_user_fields << "see_signatures"
+DiscoursePluginRegistry.serialized_current_user_fields << "disable_signatures"
 DiscoursePluginRegistry.serialized_current_user_fields << "signature_url"
 DiscoursePluginRegistry.serialized_current_user_fields << "signature_raw"
 
 after_initialize do
 
-  User.register_custom_field_type('see_signatures', :boolean)
+  User.register_custom_field_type('disable_signatures', :boolean)
   User.register_custom_field_type('signature_url', :text)
   User.register_custom_field_type('signature_raw', :text)
 
-  register_editable_user_custom_field [:see_signatures, :signature_url, :signature_raw]
+  register_editable_user_custom_field [:disable_signatures, :signature_url, :signature_raw]
 
   if SiteSetting.signatures_enabled then
     add_to_serializer(:post, :user_signature, false) {
